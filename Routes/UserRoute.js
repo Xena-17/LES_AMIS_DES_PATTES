9767f335d3c088e034512368722f4ae2b5cb1e38
Josiane <josiane.bureau17@gmail.com>
2024-07-25 15:40:16 +0200

    ajout de ClientRoutes.js

---
+++ Routes/UserRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const UserController = require('../Controllers/UserController');
+// Constante pour pouvoir specifier des routes
+const router = express.Router();
+// Toutes les routes seront précédés par /Users
+
+router.get("/", (request, result) => {UserController.getAllUser(request, result)});
+router.get("/:UserID", (request, result) => {UserController.getUserByID(request, result)});
+router.post("/", (request, result) =>{UserController.addUser(request,result)});
+router.patch("/:id", (request, result) => {UserController.updateUser(request,result)});
+router.delete("/:id", (request, result) => {UserController.deleteUser(request,result)});
+
+module.exports = router;
