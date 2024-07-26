21cdc91d90c78ff9c5522c6c609098dfadd26d1d
Josiane <josiane.bureau17@gmail.com>
2024-07-26 13:54:26 +0200

    ajout de AnimalService.js

---
+++ Services/AnimalService.js
@@ -0,0 +1,32 @@
+const Animal = require('../Models/Animal');
+
+
+
+class AnimalService {
+
+    async getAllAnimal(){
+        return await Animal.findAll();
+    }
+
+    async getAnimalByID(AnimalId){
+        return await Animal.findByPk(AnimalId);
+    }
+
+    async addUser(Animal){
+        return await Animal.create(animal)
+    }
+
+    async updateAnimal(id, animal){
+        return await Animal.update(animal, {
+            where : {
+                AN_ID : id
+            }
+        })
+    }
+
+    async deleteAnimal(id){
+        return await Animal.destroy({where : {AN_ID : id}})
+    }
+}
+
+module.exports = new AnimalService();
